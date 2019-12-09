928198ee39f4aec3167186721a5c3d865bbfeb22
fedechkoyura <fedechkoyura@gmail.com>
2019-12-09 23:28:32 +0200

    1.0003

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
     	<ul>
-			<li><NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink></li>
-			<li><NavLink to="/dialogs" activeClassName={classes.active}>Dialogs</NavLink></li>
-			<li><NavLink to="/news" activeClassName={classes.active}>News</NavLink></li>
+			<li><NavLink to="/profile" className={classes.test} activeClassName={classes.nav_link_active}>Profile</NavLink></li>
+			<li><NavLink to="/dialogs" className={classes.test} activeClassName={classes.nav_link_active}>Dialogs</NavLink></li>
+			<li><NavLink to="/news" className={classes.test} activeClassName={classes.nav_link_active}>News</NavLink></li>
 			<li><a>Music</a></li>
 			<li><a>Setting</a></li>
 		</ul>
